6fa6aebcad05330043c109be595b554ea844b747
vallem teja <vallemteja@gmail.com>
2024-03-01 17:38:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const [data, setData] = useState([]);
+
+  const fetchInfo = () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d))
+  }
+
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ color: "green" }}>using JavaScript inbuilt FETCH API</h1>
+      <center>
+        {data.map((dataObj, index) => {
+          return (
+            <div
+              style={{
+                width: "15em",
+                backgroundColor: "#35D841",
+                padding: 2,
+                borderRadius: 10,
+                marginBlock: 10,
+              }}
+            >
+              <p style={{ fontSize: 20, color: 'white' }}>{dataObj.name}</p>
+            </div>
+          );
+        })}
+      </center>
     </div>
   );
 }
